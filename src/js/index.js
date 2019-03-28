0c6e35bb4eedf8f66cbe8cfad4703e34d25ad79a
md-ahr <a.h.rony05106483@gmail.com>
2019-03-28 10:03:06 +0600

    update on cart section

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
+import {elements, renderLoader, clearLoader} from './views/base';
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
-import {elements, renderLoader, clearLoader} from './views/base';
+import * as listView from './views/listView';
 
 /*
     Global state of the app
@@ -13,7 +15,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
 */
 
 const state = {};
-
+window.state = state;
 
 
 /*
@@ -67,15 +69,18 @@ elements.searchResPages.addEventListener('click', e => {
 
 const controlRecipe = async () => {
     const id = window.location.hash.replace('#', '');
-    console.log(id);
     if(id) {
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        searchView.highlightSelected(id)    ;
         state.recipe = new Recipe(id);
         try {
             await state.recipe.getRecipe();
             state.recipe.parseIngredients();
             state.recipe.calcTime();
             state.recipe.calcServings();
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
             console.log(error);
         }
@@ -83,3 +88,50 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+
+/*
+    // Recipe Controller Model
+*/
+
+const controlList = () => {
+    if (!state.list) {
+        state.list = new List();
+    }
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+};
+
+// Handling delete and update list item
+
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        state.list.deleteItem(id);
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        state.recipe.updateServings('dec');
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
+    }
+});
+
+window.l = new List();
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -28,6 +28,7 @@ export default class Recipe {
     parseIngredients() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'kg', 'g'];
         const newIngredients = this.ingredients.map(el => {
             let ingredient = el.toLowerCase();
             unitsLong.forEach((unit, i) => {
@@ -35,7 +36,7 @@ export default class Recipe {
             });
             ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
             let objIng;
             if (unitIndex > -1) {
                 const arrCount = arrIng.slice(0, unitIndex);
@@ -66,5 +67,12 @@ export default class Recipe {
             return objIng;
         });
         this.ingredients = newIngredients;
+    }
+    updateServings(type) {
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+        this.ingredients.forEach(ing => {
+            ing.count *= (newServings / this.servings);
+        });
+        this.servings = newServings;
     }
 }
